ec185fc8aea585dd6a143cecb293af9a29edd101
Im-Rises <quentin-morel88@hotmail.com>
2023-05-26 19:10:40 -0400

    wip

--- src/components/InputRange.tsx
+++ src/components/InputRange.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './InputRange.scss';
 
 type Props = {
 	inputRef: React.RefObject<HTMLInputElement>;
--- src/components/TextAreaInputOutput.tsx
+++ src/components/TextAreaInputOutput.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './TextAreaInputOutput.scss';
 
 type Props = {
 	paragraphRef: React.RefObject<HTMLParagraphElement>;
