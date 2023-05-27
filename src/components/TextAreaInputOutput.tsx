5db0375eafb0919a7838169bff683be144754f02
Im-Rises <quentin-morel88@hotmail.com>
2023-05-27 02:01:56 -0400

    wip

--- src/components/TextAreaInputOutput.tsx
+++ src/components/TextAreaInputOutput.tsx
@@ -6,11 +6,12 @@ type Props = {
 	actionButtonFunc: () => void;
 	handleCopyFunc: (text: string) => void;
 	actionText: string;
+	maxLength: number;
 };
 export const TextAreaInputOutput = (props: Props) => (
 	<div className={'text-area-paragraph-holder'}>
 		<div className={'text-area-paragraph'}>
-			<textarea ref={props.textareaRef}></textarea>
+			<textarea ref={props.textareaRef} maxLength={props.maxLength}></textarea>
 		</div>
 		<div className={'text-area-button'}>
 			<button onClick={props.actionButtonFunc}>{props.actionText}</button>
